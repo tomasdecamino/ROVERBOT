f9e5ec740e9a9fa29196797eaeb29e2e212dc0c0
Alex Vargas Benamburg <alxelaex@gmail.com>
2015-11-16 20:38:47 -0600

    Ahora compila

--- PID.cpp
+++ PID.cpp
@@ -23,26 +23,26 @@
 
 PID::PID(float Kc, float tauI, float tauD, float interval) {
 
-    usingFeedForward = false;
-    inAuto           = false;
+    this->usingFeedForward = false;
+    this->inAuto           = false;
 
-    setInputLimits(-5.0, 5.0);
-    setOutputLimits(-1.0, 1.0);
+    this->setInputLimits(-5.0, 5.0);
+    this->setOutputLimits(0.0, 2.0);
 
-    tSample_ = interval;
+    this->tSample = interval;
 
-    setTunings(Kc, tauI, tauD);
+    this->setTunings(Kc, tauI, tauD);
 
-    setPoint_             = 0.0;
-    processVariable_      = 0.0;
-    prevProcessVariable_  = 0.0;
-    controllerOutput_     = 0.0;
-    prevControllerOutput_ = 0.0;
+    this->setPoint             = 0.0;
+    this->processVariable      = 0.0;
+    this->prevProcessVariable  = 0.0;
+    this->controllerOutput     = 0.0;
+    this->prevControllerOutput = 0.0;
 
-    accError_ = 0.0;
-    bias_     = 0.0;
+    this->accError = 0.0;
+    this->bias     = 0.0;
     
-    realOutput_ = 0.0;
+    this->realOutput = 0.0;
 
 }
 
@@ -54,42 +54,39 @@ void PID::setInputLimits(float inMin, float inMax) {
     }
 
     //Rescale the working variables to reflect the changes.
-    prevProcessVariable_ *= (inMax - inMin) / inSpan_;
-    accError_            *= (inMax - inMin) / inSpan_;
+    this->prevProcessVariable *= (inMax - inMin) / this->inSpan;
+    this->accError            *= (inMax - inMin) / this->inSpan;
 
     //Make sure the working variables are within the new limits.
-    if (prevProcessVariable_ > 1) {
-        prevProcessVariable_ = 1;
-    } else if (prevProcessVariable_ < 0) {
-        prevProcessVariable_ = 0;
+    if (this->prevProcessVariable > 1) {
+        this->prevProcessVariable = 1;
+    } else if (this->prevProcessVariable < 0) {
+        this->prevProcessVariable = 0;
     }
 
-    inMin_  = inMin;
-    inMax_  = inMax;
-    inSpan_ = inMax - inMin;
+    this->inMin  = inMin;
+    this->inMax  = inMax;
+    this->inSpan = inMax - inMin;
 
 }
 
 void PID::setOutputLimits(float outMin, float outMax) {
-
     //Make sure we haven't been given impossible values.
     if (outMin >= outMax) {
         return;
     }
 
     //Rescale the working variables to reflect the changes.
-    prevControllerOutput_ *= (outMax - outMin) / outSpan_;
-
-    //Make sure the working variables are within the new limits.
-    if (prevControllerOutput_ > 1) {
-        prevControllerOutput_ = 1;
-    } else if (prevControllerOutput_ < 0) {
-        prevControllerOutput_ = 0;
+    this->prevControllerOutput *= (outMax - outMin) / this->outSpan;
+	//Make sure the working variables are within the new limits.
+    if (this->prevControllerOutput > 1) {
+        this->prevControllerOutput = 1;
+    } else if (this->prevControllerOutput < 0) {
+        this->prevControllerOutput = 0;
     }
-
-    outMin_  = outMin;
-    outMax_  = outMax;
-    outSpan_ = outMax - outMin;
+    this->outMin  = outMin;
+    this->outMax  = outMax;
+    this->outSpan = outMax - outMin;
 
 }
 
@@ -101,98 +98,86 @@ void PID::setTunings(float Kc, float tauI, float tauD) {
     }
 
     //Store raw values to hand back to user on request.
-    pParam_ = Kc;
-    iParam_ = tauI;
-    dParam_ = tauD;
+    this->pParam = Kc;
+    this->iParam = tauI;
+    this->dParam = tauD;
 
     float tempTauR;
 
     if (tauI == 0.0) {
         tempTauR = 0.0;
     } else {
-        tempTauR = (1.0 / tauI) * tSample_;
+        tempTauR = (1.0 / tauI) * this->tSample;
     }
 
     //For "bumpless transfer" we need to rescale the accumulated error.
     if (inAuto) {
         if (tempTauR == 0.0) {
-            accError_ = 0.0;
+            this->accError = 0.0;
         } else {
-            accError_ *= (Kc_ * tauR_) / (Kc * tempTauR);
+            this->accError *= (this->Kc * this->tauR) / (Kc * tempTauR);
         }
     }
 
-    Kc_   = Kc;
-    tauR_ = tempTauR;
-    tauD_ = tauD / tSample_;
+    this->Kc   = Kc;
+    this->tauR = tempTauR;
+    this->tauD = tauD / this->tSample;
 
 }
 
 void PID::reset(void) {
-
     float scaledBias = 0.0;
-
-    if (usingFeedForward) {
-        scaledBias = (bias_ - outMin_) / outSpan_;
+    if (this->usingFeedForward) {
+        scaledBias = (this->bias - this->outMin) / this->outSpan;
     } else {
-        scaledBias = (realOutput_ - outMin_) / outSpan_;
+        scaledBias = (this->realOutput - this->outMin) / this->outSpan;
     }
 
-    prevControllerOutput_ = scaledBias;
-    prevProcessVariable_  = (processVariable_ - inMin_) / inSpan_;
+    this->prevControllerOutput = scaledBias;
+    this->prevProcessVariable  = (this->processVariable - this->inMin) / this->inSpan;
 
     //Clear any error in the integral.
-    accError_ = 0;
-
+    this->accError = 0;
 }
 
 void PID::setMode(int mode) {
 
     //We were in manual, and we just got set to auto.
     //Reset the controller internals.
-    if (mode != 0 && !inAuto) {
-        reset();
+    if (mode != 0 && !this->inAuto) {
+        this->reset();
     }
 
-    inAuto = (mode != 0);
+    this->inAuto = (mode != 0);
 
 }
 
 void PID::setInterval(float interval) {
-
     if (interval > 0) {
         //Convert the time-based tunings to reflect this change.
-        tauR_     *= (interval / tSample_);
-        accError_ *= (tSample_ / interval);
-        tauD_     *= (interval / tSample_);
-        tSample_   = interval;
+        this->tauR     *= (interval / this->tSample);
+        this->accError *= (this->tSample / interval);
+        this->tauD     *= (interval / this->tSample);
+        this->tSample   = interval;
     }
-
 }
 
 void PID::setSetPoint(float sp) {
-
-    setPoint_ = sp;
-
+    this->setPoint = sp;
 }
 
 void PID::setProcessValue(float pv) {
-
-    processVariable_ = pv;
-
+    this->processVariable = pv;
 }
 
 void PID::setBias(float bias){
-
-    bias_ = bias;
-    usingFeedForward = 1;
-
+    this->bias = bias;
+    this->usingFeedForward = 1;
 }
 
 float PID::compute() {
-
     //Pull in the input and setpoint, and scale them into percent span.
-    float scaledPV = (processVariable_ - inMin_) / inSpan_;
+    float scaledPV = (this->processVariable - this->inMin) / this->inSpan;
 
     if (scaledPV > 1.0) {
         scaledPV = 1.0;
@@ -200,7 +185,7 @@ float PID::compute() {
         scaledPV = 0.0;
     }
 
-    float scaledSP = (setPoint_ - inMin_) / inSpan_;
+    float scaledSP = (this->setPoint - this->inMin) / this->inSpan;
     if (scaledSP > 1.0) {
         scaledSP = 1;
     } else if (scaledSP < 0.0) {
@@ -211,67 +196,51 @@ float PID::compute() {
 
     //Check and see if the output is pegged at a limit and only
     //integrate if it is not. This is to prevent reset-windup.
-    if (!(prevControllerOutput_ >= 1 && error > 0) && !(prevControllerOutput_ <= 0 && error < 0)) {
-        accError_ += error;
+    if (!(this->prevControllerOutput >= 1 && error > 0) && !(this->prevControllerOutput <= 0 && error < 0)) {
+        this->accError += error;
     }
 
     //Compute the current slope of the input signal.
-    float dMeas = (scaledPV - prevProcessVariable_) / tSample_;
+    float dMeas = (scaledPV - this->prevProcessVariable) / this->tSample;
 
     float scaledBias = 0.0;
 
-    if (usingFeedForward) {
-        scaledBias = (bias_ - outMin_) / outSpan_;
+    if (this->usingFeedForward) {
+        scaledBias = (this->bias - this->outMin) / this->outSpan;
     }
 
     //Perform the PID calculation.
-    controllerOutput_ = scaledBias + Kc_ * (error + (tauR_ * accError_) - (tauD_ * dMeas));
+    this->controllerOutput = scaledBias + this->Kc * (error + (this->tauR * this->accError) - (this->tauD * dMeas));
 
     //Make sure the computed output is within output constraints.
-    if (controllerOutput_ < 0.0) {
-        controllerOutput_ = 0.0;
-    } else if (controllerOutput_ > 1.0) {
-        controllerOutput_ = 1.0;
+    if (this->controllerOutput < 0.0) {
+        this->controllerOutput = 0.0;
+    } else if (this->controllerOutput > 1.0) {
+        this->controllerOutput = 1.0;
     }
 
     //Remember this output for the windup check next time.
-    prevControllerOutput_ = controllerOutput_;
+    this->prevControllerOutput = this->controllerOutput;
     //Remember the input for the derivative calculation next time.
-    prevProcessVariable_  = scaledPV;
+    this->prevProcessVariable  = scaledPV;
 
     //Scale the output from percent span back out to a real world number.
-    return ((controllerOutput_ * outSpan_) + outMin_);
+    return ((this->controllerOutput * this->outSpan) + this->outMin);
 
 }
 
-float PID::getInMin() {
+float PID::getInMin() { return this->inMin; }
 
-    return inMin_;
+float PID::getInMax() { return this->inMax; }
 
-}
+float PID::getOutMin() { return this->outMin;}
 
-float PID::getInMax() {
+float PID::getOutMax() { return this->outMax; }
 
-    return inMax_;
+float PID::getInterval() { return this->tSample; }
 
-}
+float PID::getP() { return this->pParam; }
 
-float PID::getOutMin() {
+float PID::getI() { return this->iParam; }
 
-    return outMin_;
-
-}
-
-float PID::getOutMax() {
-
-    return outMax_;
-
-}
-
-float PID::getInterval() { return tSample; }
-
-float PID::getP() { return pParam; }
-
-float PID::getI() { return iParam; }
-
-float PID::getD() { return dParam; }
+float PID::getD() { return this->dParam; }
