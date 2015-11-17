b27278989ccc28fc949025ba48261716e4e9c5eb
Alex Vargas Benamburg <alxelaex@gmail.com>
2015-11-16 19:48:56 -0600

    Clase para manejo de L298N(Controladores de motores)

---
+++ Traccion.cpp
@@ -0,0 +1,69 @@
+// Traccion.cpp
+// 
+// Copyright 2015 Alex Vargas Benamburg <alex@algecr.tk>
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+// MA 02110-1301, USA.
+// 
+// 
+
+#include "Traccion.h"
+Traccion::Traccion(
+            PinName iv, PinName id,
+            PinName dv, PinName dd
+            ):
+                    dirI(id),velI(iv),
+                    dirD(dd),velD(dv){
+}
+
+Traccion::~Traccion(){}
+
+void Traccion::init(void){
+    this->velI.write(0);
+    this->velI.period_us(PWM_TRACCION_PERIOD);
+    this->velD.write(0);
+    this->velD.period_us(PWM_TRACCION_PERIOD);
+    this->dirI.write(0);
+    this->dirD.write(0);
+    this->stop();
+}
+
+void Traccion::stop(bool brute){
+    if(brute){
+        velI.write(0);
+        velD.write(0);
+        dirI.write(0);
+        dirD.write(0);
+    }else{
+        //TODO
+    }
+}
+
+void Traccion::set(float veli,float veld){
+    if(veld<0.0f){
+        this->dirD = 1;
+        veld = 1.0f+veld;
+    }else{
+        this->dirD = 0;
+    }
+    if(veli<0.0f){
+        this->dirI = 1;
+        veli = 1.0f+veli;
+    }else{
+        this->dirI = 0;
+    }
+    this->velI = veli;
+    this->velD = veld;
+}
---
+++ include/Traccion.h
@@ -0,0 +1,52 @@
+// Traccion.h
+// 
+// Copyright 2015 Alex Vargas Benamburg <alex@algecr.tk>
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+// MA 02110-1301, USA.
+// 
+// 
+
+
+#ifndef TRACCION_H
+#define TRACCION_H
+
+#include "mbed.h"
+#include "PwmOut.h"
+#include "DigitalOut.h"
+    
+#define PWM_TRACCION_PERIOD 500 // 10000/5 = 2KHz   
+    
+class Traccion{
+    public:
+        Traccion(
+            PinName iv, PinName id,
+            PinName dv, PinName dd
+        );
+        void init(void);
+        void stop(bool brute=true);
+        void set(float veli,float veld);
+        
+        ~Traccion(void);
+            
+    private:
+        DigitalOut dirI;
+        PwmOut velI;
+        DigitalOut dirD;
+        PwmOut velD;
+
+};
+
+#endif /* TRACCION_H */ 
