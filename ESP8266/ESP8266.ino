49049a504784b9c9409ec296f9b319cc90ad6481
Alex Vargas <alxelaex@gmail.com>
2015-11-22 09:09:02 -0600

    Modificacion para trabajar con UDP mayor velocidad de respuesta y trabajo mas limpio.

--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -1,63 +1,47 @@
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h> 
-#include <ESP8266WebServer.h>
+#include <WiFiUdp.h>
+
+unsigned int port = 2390;
 
 const char *ssid = "ROVER";
 const char *password = "itisopen";
 
-const char *vi = "vi";
-const char *vd = "vd";
-
-volatile int velI;
-volatile int velD;
-
-String angulos;
-String temp;
+char angulos[128];
 int indice;
-char buffer[128];
+int packetSize;
+char buffer[255];
 
-ESP8266WebServer server(80);
-void handleRoot() {
-  indice = server.args();
-  for(;indice>=0;--indice){
-    temp = server.argName(indice);
-    if(temp == vi){
-      velI =  server.arg(indice).toInt();
-    }else{
-      if(temp == vd){
-        velD = server.arg(indice).toInt();
-      } 
-    }
-  }  
-	server.send(200, "text/html", angulos);
-  Serial.print(' ');
-  delay(10);
-  Serial.print(velI);
-  Serial.print(',');
-  Serial.print(velD);
-  Serial.print(' ');
-  
-}
+WiFiUDP Udp;
 
 void setup() {
-  pinMode(13,OUTPUT);
-  digitalWrite(13,LOW);
-	delay(1000);
-	Serial.begin(115200);
-	WiFi.softAP(ssid, password);
-  Serial.println(WiFi.softAPIP());
-	
-	server.on("/", handleRoot);
-	server.begin();
-  Serial.flush();
-  digitalWrite(13,HIGH);
+  Serial.begin(115200);
+  WiFi.softAP(ssid, password);
+
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.println(myIP);
+ 
+  Udp.begin(port);
 }
 
 void loop() {
-	server.handleClient();
+  packetSize = Udp.parsePacket();
+  if (packetSize) {
+    indice = Udp.read(buffer, 255);
+    if (indice > 0) {
+      buffer[indice] = 0;
+    }
+    Serial.print(' ');
+    delay(1);
+    Serial.print(buffer);
+    delay(1);
+    Serial.print(' ');
+    
+    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    Udp.write(angulos);
+    Udp.endPacket();
+  }
   if(Serial.available()){
-    indice = Serial.readBytesUntil('s', buffer, 128);
-    buffer[indice]='\0';
-    angulos = buffer; 
+    indice = Serial.readBytesUntil('s', angulos, 255);
+    angulos[indice]='\0';
   }
 }
