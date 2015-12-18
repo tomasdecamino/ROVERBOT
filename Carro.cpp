a69edf89d2001b5e4e03e63f31fd0a951ba158ed
Tomas de Camino Beck <tomas.decamino@gmail.com>
2015-12-17 19:31:29 -0600

    cambio para probar

--- Carro.cpp
+++ Carro.cpp
@@ -1,7 +1,7 @@
 // Carro.cpp
 // 
 // Copyright 2015 Alex Vargas Benamburg <alex@algecr.tk>
-// 
+// Cambio de Tomas de  PRueba 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation; either version 2 of the License, or
